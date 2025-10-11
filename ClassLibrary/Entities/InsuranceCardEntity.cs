7855fa80709f7526f9a671b0bb5e0156db1a80dc
Shafaqat Ali <shafaqat.ali@cms365.com.au>
2025-10-11 13:23:34 +1000

    insurance card processing

---
+++ ClassLibrary/Entities/InsuranceCardEntity.cs
@@ -0,0 +1,14 @@
+﻿namespace ClassLibrary.Entities
+{
+    public class InsuranceCardEntity
+    {
+        public IdNumber IdNumber { get; set; }
+        public string Insurer { get; set; }
+        public MedicareMedicaidInfo MedicareMedicaidInfo { get; set; }
+        public Member Member { get; set; }
+        public DateTimeOffset EffectiveDate { get; set; }
+        public PrescriptionInfo PrescriptionInfo { get; set; }
+        public Subscriber Subscriber { get; set; }
+        public Plan Plan { get; set; }
+    }
+}
